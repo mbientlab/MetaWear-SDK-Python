e972fb5d2e540d4c25f4df628cd771dc5bca7868
lkasso <laura@kassovic.com>
2021-01-03 13:11:34 -0800

    setup and readme from 0.8.2 release

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ so_pkg_data = ['libmetawear.so']
 setup(
     name='metawear',
     packages=['mbientlab', 'mbientlab.metawear'],
-    version='0.8.0',
+    version='0.8.2',
     description='Python bindings for the MetaWear C++ SDK by MbientLab',
     long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
     package_data={'mbientlab.metawear': so_pkg_data},
